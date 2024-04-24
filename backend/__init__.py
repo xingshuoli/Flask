4fe09a4498b6719fb205b80ac8dc9c3c9ad569ee
lixingshuo <lixingshuo@gotion.com.cn>
2024-04-24 17:25:11 +0800

    add

---
+++ backend/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/4/24 下午5:24
+@Auth ： lixingshuo
+@File ：__init__.py.py
+@IDE ：PyCharm
+@mail ： lixingshuo@gotion.com.cn
+"""
---
+++ backend/preprocess.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2024/4/24 下午5:24
+@Auth ： lixingshuo
+@File ：preprocess.py
+@IDE ：PyCharm
+@mail ： lixingshuo@gotion.com.cn
+"""
